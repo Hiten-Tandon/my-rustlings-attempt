76a9e4e54840154a88c4beac93f517e2c8d83a63
Hiten Tandon <tandonhiten@gmail.com>
2023-06-29 21:04:04 +0530

    Feat: Added a few solutions

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -5,17 +5,14 @@
 // construct to `Option` that can be used to express error conditions. Let's use it!
 // Execute `rustlings hint errors1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::ops::Not;
 
-pub fn generate_nametag_text(name: String) -> Option<String> {
-    if name.is_empty() {
-        // Empty names aren't allowed.
-        None
-    } else {
-        Some(format!("Hi! My name is {}", name))
-    }
+pub fn generate_nametag_text(name: String) -> Result<String, String> {
+    name.is_empty()
+        .not()
+        .then_some(format!("Hi! My name is {}", name))
+        .ok_or(String::from("`name` was empty; it must be nonempty."))
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,8 +1,6 @@
 // errors4.rs
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -14,8 +12,15 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        u64::try_from(value)
+            .map_err(|_| CreationError::Negative)
+            .and_then(|val| {
+                if val == 0 {
+                    Err(CreationError::Zero)
+                } else {
+                    Ok(PositiveNonzeroInteger(val))
+                }
+            })
     }
 }
 
